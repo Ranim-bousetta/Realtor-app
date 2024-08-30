b4cf1df71930a638be290c4fa07cfe36d067385c
Ranim Bousetta <bousetta.ranim@gmail.com>
2024-08-30 14:33:55 +0100

    create pages and routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import SingIn from "./pages/SingIn"
+import SignUp from "./pages/SignUp"
+import Offers from "./pages/Offers"
+import ForgotPassword from "./pages/ForgotPassword"
 
 function App() {
   return (
     <>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/profile" element={<Profile/>} />
+            <Route path="/sign-in" element={<SingIn/>} />
+            <Route path="/sign-up" element={<SignUp/>} />
+            <Route path="/forgot-password" element={<ForgotPassword/>} />
+            <Route path="/offers" element={<Offers/>} />
+        </Routes>
+      </Router>
       
     </>
   );
