e782a6e180ba7d77df7c6fc3778051045b3d3037
Ranim Bousetta <bousetta.ranim@gmail.com>
2024-09-11 09:58:09 +0100

    fix header style

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   const location= useLocation();
   const navigate=useNavigate();
   console.log(location);
-  function pathMathRoute(route){
+  function pathMatchRoute(route){
     if (route=== location.pathname){
       return true;
     }
@@ -21,13 +21,19 @@ function Header() {
         <div>
           <ul className='flex space-x-10 '>
             <li 
-            className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute('/') && "text-black border-b-red-500 "}`}
+            className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+              pathMatchRoute("/") && "!text-black !border-b-red-500"
+            }`}
             onClick={()=>navigate("/")}
             >Home</li>
-            <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute('/offers') && "text-black border-b-red-500 "}`}
+            <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/offers") && "!text-black !border-b-red-500"
+              }`}
             onClick={()=>navigate("/offers")}
             >Offers</li>
-            <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${pathMathRoute('/sign-in') && "text-black border-b-red-500 "}`}
+            <li className={`py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMatchRoute("/sign-in") && "!text-black !border-b-red-500"
+              }`}
             onClick={()=>navigate("/sign-in")}
             >Sign In</li>
           </ul>
