49982424c7ff5051fa87a2deef168f9f442ae437
Ranim Bousetta <bousetta.ranim@gmail.com>
2024-09-12 11:21:02 +0100

    minor fix

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ function App() {
         <Header/>
         <Routes>
             <Route path="/" element={<Home/>} />
-            <Route path="/profile" element={<PrivateRoute />} />
+            <Route path="/profile" element={<PrivateRoute />}>
             <Route path="/profile" element={<Profile/>} />
+            </Route>  
             <Route path="/sign-in" element={<SignIn/>} />
             <Route path="/sign-up" element={<SignUp/>} />
             <Route path="/forgot-password" element={<ForgotPassword/>} />
