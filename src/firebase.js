666a291491640c5aa0308e3324288081377a64bb
Ranim Bousetta <bousetta.ranim@gmail.com>
2024-09-13 17:16:12 +0100

    security changes

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDBSvhtUotbd8d0Y_gnN0rolc5y3t65GZk",
-  authDomain: "realtor-app-react-26b12.firebaseapp.com",
-  projectId: "realtor-app-react-26b12",
-  storageBucket: "realtor-app-react-26b12.appspot.com",
-  messagingSenderId: "102710604703",
-  appId: "1:102710604703:web:c005f9033f8dd735fd43aa"
+  apiKey:  process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain:  process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId:  process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket:  process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId:  process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId:  process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 initializeApp(firebaseConfig);
 export const db =getFirestore()
+
