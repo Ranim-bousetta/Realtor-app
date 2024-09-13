70f3243f2272f9a5e1395013c36261412e6e72ad
Ranim Bousetta <bousetta.ranim@gmail.com>
2024-09-13 09:39:27 +0100

    create the create listing page UI

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,6 +5,8 @@ import { useState } from 'react';
 import { doc, updateDoc } from 'firebase/firestore';
 import { toast } from 'react-toastify';
 import {db} from "../firebase"
+import { FcHome } from "react-icons/fc";
+import { Link } from 'react-router-dom';
 export default function Profile() {
   const auth= getAuth()
   const navigate=useNavigate()
@@ -81,6 +83,16 @@ export default function Profile() {
               transition ease-in-out duration-200 cursor-pointer">Sign Out</p>
             </div>
           </form>
+          <button type='submit'
+          className='w-full h-11 bg-blue-600 text-white uppercase 
+          px-7 text-sm font-medium rounded shadow-md hover:bg-blue-700 
+          transition duration-150 ease-in-out hover:shadow:lg active:bg-blue-900 '
+          >
+            <Link to="/create-listing" className="flex justify-center items-center">
+            <FcHome className='mr-2 text-3xl bg-red-200 rounded-full p-1 border-2 ' />
+            sell or rent your home
+            </Link> 
+          </button>
         </div>
       </section>
     </>
