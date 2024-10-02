d5db3cadbc4c9d1be399c74769e0f52239f92d12
Ranim Bousetta <bousetta.ranim@gmail.com>
2024-10-02 21:31:49 +0100

    add functionality including onchange and onsubmit function to the create listing page

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -3,7 +3,18 @@ import React from 'react'
 import { useState } from 'react'
 import Spinner from '../components/Spinner';
 import { toast } from 'react-toastify';
+import {getAuth} from "firebase/auth";
+import {v4 as uuidv4} from "uuid";
+import {useNavigate} from "react-router-dom";
+import {addDoc,collection, serverTimestamp} from "firebase/firestore";
+import {db} from "../firebase"
+import { getStorage, 
+    ref, 
+    uploadBytesResumable, 
+    getDownloadURL } from "firebase/storage";
 export default function CreateListing() {
+    const navigate= useNavigate();
+    const auth = getAuth()
     const [geolocationEnabled,setGeolocationEnabled]=useState(true);
     const[loading,setLoading]=useState(false)
     const [formData,setFormData]=useState({
@@ -60,12 +71,12 @@ export default function CreateListing() {
                 [e.target.id]: boolean ?? e.target.value,
             }))
         }
- }
+    }
 
  async function onSubmit(e){
     e.preventDefault();
     setLoading(true);
-    if(discountedPrice>=regularPrice){
+    if(+discountedPrice>= +regularPrice){
         setLoading(false)
         toast.error("Discounted price need to be less than regular price")
         return;
@@ -77,13 +88,91 @@ export default function CreateListing() {
     }
     let geolocation={}
     let location
-    if (geolocationEnabled){
-        const response = await fetch(`http://ip-api.com/json/${address}`)
-        const data=await response.json()
+    if (geolocationEnabled) {
+        const response = await fetch(
+          `https://nominatim.openstreetmap.org/search?format=json&q=${address}`
+        );
+        const data = await response.json();
         console.log(data);
         
+        if (data.length > 0) {
+          geolocation.lat = data[0]?.lat ?? 0;
+          geolocation.lng = data[0]?.lon ?? 0; 
+          location = true;
+        } else {
+          location = undefined;
+        }
+      
+        if (location === undefined) {
+          setLoading(false);
+          toast.error("Please enter a correct address");
+          return;
+        }
+    } else {
+        geolocation.lat = latitude;
+        geolocation.lng = longitude;
     }
- }
+    async function storeImage(image){
+        return new Promise((resolve,reject)=>{
+            const storage=getStorage();
+            const filename=`${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+            const storageRef = ref(storage, filename);
+            const uploadTask = uploadBytesResumable(storageRef, image);
+            uploadTask.on('state_changed', 
+                (snapshot) => {
+                  // Observe state change events such as progress, pause, and resume
+                  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                  const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                  console.log('Upload is ' + progress + '% done');
+                  switch (snapshot.state) {
+                    case 'paused':
+                      console.log('Upload is paused');
+                      break;
+                    case 'running':
+                      console.log('Upload is running');
+                      break;
+                  }
+                }, 
+                (error) => {
+                  // Handle unsuccessful uploads
+                  reject(error)
+                }, 
+                () => {
+                  // Handle successful uploads on complete
+                  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                  getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                    resolve(downloadURL);
+                  });
+                }
+              );
+        });
+    }
+
+
+ const imgUrls = await Promise.all(
+    [...images].map((image) => storeImage(image))
+  ).catch((error) => {
+    setLoading(false);
+    toast.error("Images not uploaded");
+    return;
+  });
+  const formDataCopy = {
+    ...formData,
+    imgUrls,
+    geolocation,
+    timestamp: serverTimestamp(),
+    userRef: auth.currentUser.uid,
+  };
+  delete formDataCopy.images;
+  !formDataCopy.offer && delete formDataCopy.discountedPrice;
+  delete formDataCopy.latitude;
+  delete formDataCopy.longitude;
+  const docRef = await addDoc(collection(db, "listings"), formDataCopy);
+  setLoading(false);
+  toast.success("Listing created");
+  navigate(`/category/${formDataCopy.type}/${docRef.id}`);
+  
+}
  if(loading){
     return <Spinner/>
  }
