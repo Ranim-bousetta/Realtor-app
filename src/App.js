f0ebb8df4cccef872edd7085338208c9a8d56ffc
Ranim Bousetta <bousetta.ranim@gmail.com>
2024-10-11 11:43:24 +0100

    create category pages and add it to app.js hence complete project

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import ForgotPassword from "./pages/ForgotPassword"
 import Header from "./components/Header"
 import EditListing from "./pages/EditListing";
 import Listing from "./pages/Listing";
+import Category from "./pages/Category";
 import { ToastContainer } from "react-toastify"
 import 'react-toastify/dist/ReactToastify.css';
 import CreateListing from "./pages/CreateListing"
@@ -27,6 +28,7 @@ function App() {
             <Route path="/forgot-password" element={<ForgotPassword/>} />
             <Route path="/category/:categoryName/:listingId" element={<Listing />} />
             <Route path="/offers" element={<Offers/>} />          
+            <Route path="/category/:categoryName" element={<Category />} />
             <Route path="/create-listing" element={<PrivateRoute/>}>
               <Route path="/create-listing" element={<CreateListing/>} />
             </Route>
